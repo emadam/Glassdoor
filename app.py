59b5c240e4bed6848a699fe6d2645b8a2e30b55b
Emad Aminmoghadam <emadam@gmail.com>
2022-03-29 02:01:23 +1100

    app.py changed

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ SERVER_NAME = os.getenv('server')
 DB_NAME = os.getenv('database')
 USERNAME = os.getenv("pymssql_username")
 PASSWORD = os.getenv("pymssql_password")
-HOST_IP = '172.21.192.1' #find_host_ip()
+HOST_IP = '2401:d1c0:2c02:b1c:2575:d896:abc1:e097'  #find_host_ip()
 
 headers = {
     "User-Agent":
